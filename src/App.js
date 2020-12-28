7bc69bd80ecde0399e38427284334324fa57f783
newt0 <py_lambda@yahoo.co.jp>
2020-12-28 18:35:28 +0900

    add: setup Webcam and  canvas

--- src/App.js
+++ src/App.js
@@ -1,5 +1,20 @@
+import { React, useRef } from "react";
+import "./App.css";
+import Webcam from "react-webcam";
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+
 function App() {
-  return <div></div>;
+  const webcamRef = useRef(null);
+  const canvasRef = useRef(null);
+  return (
+    <div className="app">
+      <div className="app_header">
+        <Webcam className="webcam" ref={webcamRef} />
+        <canvas className="canvas" ref={canvasRef} />
+      </div>
+    </div>
+  );
 }
 
 export default App;
