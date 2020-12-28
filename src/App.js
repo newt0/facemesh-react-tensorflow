b7f3fe25aea95013589c32ffe442dd52e725f5da
newt0 <py_lambda@yahoo.co.jp>
2020-12-28 19:01:29 +0900

    add: detect function

--- src/App.js
+++ src/App.js
@@ -15,7 +15,42 @@ function App() {
       inputResolution: { width: 640, height: 480 },
       scale: 0.8,
     });
+    setInterval(() => {
+      detect(net);
+    }, [100]); // run detect function every 100 milliseconds
   };
+
+  // detect function
+  const detect = async (net) => {
+    if (
+      // checking the webcam is up and running, and we're receiving data
+      typeof webcamRef.current !== "undefined" &&
+      webcamRef.current !== null &&
+      webcamRef.current.video.readyState === 4
+    ) {
+      // get video properties
+      const video = webcamRef.current.video;
+      const videoWidth = webcamRef.current.videoWidth;
+      const videoHeight = webcamRef.current.videoHeight;
+
+      // set video width
+      webcamRef.current.video.width = videoWidth;
+      webcamRef.current.video.height = videoHeight;
+
+      // set canvas width
+      canvasRef.current.width = videoWidth;
+      canvasRef.current.height = videoHeight;
+
+      // make detections
+      const face = await net.estimateFaces(video);
+      console.log(face);
+
+      // get canvas context for drawing
+    }
+  };
+
+  runFacemesh();
+
   return (
     <div className="app">
       <div className="app_header">
